b782f798760e00fa3c14004ce331704ef9102471
Geert Jansen <geert@MacBook-Pro-van-Geert.local>
2022-02-14 10:56:44 +0100

    adds sticky note and youtube player examples, adds start script.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Component Examples
+# IXON Custom Component Examples
 
+This repository contains examples of custom components built with the IXON Component SDK. It may be used to gain insight or inspiration or serve as a template for your own custom components.
+
+## Getting started
+
+Install the dependencies...
+
+```sh
+npm install
+```
+
+Now you can demo an example by running:
+
+```sh
+npm start
+```
+
+You will be prompted to select a specific example, which will then be built and opened in the simulator in a browser.
---
+++ examples/sticky-note/sticky-note.vue
@@ -0,0 +1,36 @@
+<template>
+  <main :style="{ backgroundColor: bgColor }">
+    <p>{{ text }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'sticky-note',
+  props: ['context'],
+  computed: {
+    bgColor() {
+      return this.context.inputs.color;
+    },
+    text() {
+      return this.context.inputs.text;
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  box-sizing: border-box;
+  height: 100%;
+  padding: 1em;
+  background-color: #feff9c;
+}
+p {
+  margin: 0;
+  font-family: "Marker Felt", "Brush Script MT", cursive;
+  font-size: 2.5em;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.87);
+}
+</style>
