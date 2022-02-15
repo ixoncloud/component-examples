acc180e5fc46a927ee19e251ddcc0ab834d10277
Joris Bloemen <joris.bloemen@ixon.cloud>
2022-02-15 17:14:48 +0100

    Added vue google-maps example

---
+++ examples/google-maps/google-maps.vue
@@ -0,0 +1,35 @@
+<template>
+  <GoogleMap
+    api-key="YOUR_API_KEY_HERE"
+    style="width: 100%; height: 100%"
+    :center="center"
+    :zoom="zoom"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script>
+import { GoogleMap, Marker } from 'vue3-google-map';
+
+export default {
+  name: 'google-maps',
+  props: ['context'],
+  components: { GoogleMap, Marker },
+  data() {
+    return {
+      center: {
+        lat: 40.689247,
+        lng: -74.044502,
+      },
+      zoom: 15,
+    };
+  },
+};
+</script>
+
+<style scoped>
+::v-deep .mapdiv {
+  height: 100%;
+}
+</style>
